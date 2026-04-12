37002d81f3f569e9894980f76f578ba9ddb5418c
osertkaya <sertkaya93@gmail.com>
2026-04-12 16:08:08 +0200

    add aider auto installation

--- nvim/lua/deps.lua
+++ nvim/lua/deps.lua
@@ -4,7 +4,8 @@ function M.install(venv_dir)
   local exists = os.execute("[ -d " .. venv_dir .. " ]") == 0
   if not exists then
     os.execute("python3 -m venv " .. venv_dir)
-    os.execute(venv_dir .. "/bin/python3 -m pip install ruff ty")
+    os.execute(venv_dir .. "/bin/python3 -m pip install aider-install ruff ty")
+    os.execute(venv_dir .. "/bin/aider-install")
   end
 end
 
